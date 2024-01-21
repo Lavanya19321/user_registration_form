049e382c833fd47e4841ec085cb45fc7870a479a
somulavanya <lavanyayaddalpudi@gmail.com>
2024-01-20 18:58:16 -0800

    first commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from app.forms import *
 
@@ -8,4 +9,20 @@ def register(request):
     UFO=UserForm()
     PFO=ProfileForm()
     d={'UFO':UFO,'PFO':PFO}
+    if request.method=='POST' and request.FILES :
+        UFD=UserForm(request.POST)
+        PFD=ProfileForm(request.POST,request.FILES)
+        if UFD.is_valid() and PFD.is_valid():
+            MUFDO=UFD.save(commit=False)
+            pw=UFD.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            
+            MPFDO=PFD.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            return HttpResponse('register successfully')
+        else:
+            return HttpResponse('invalid')
+
     return render(request,'register.html',d)
--- project10/urls.py
+++ project10/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register',register,name='register'),
-]
+
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
